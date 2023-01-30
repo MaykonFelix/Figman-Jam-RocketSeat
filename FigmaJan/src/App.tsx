d417474cea6a42abb111acb4eb481d201637fb25
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-01-30 13:42:54 -0300

    Update App.tsx

--- FigmaJan/src/App.tsx
+++ FigmaJan/src/App.tsx
@@ -89,7 +89,10 @@ function App() {
       </ReactFlow>
 
       <Toolbar.Root className="fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border-zinc-300 px-8 h-20 w-96 overflow-hidden">
-        <Toolbar.Button onClick={addSquareNode} className="w-32 h32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2" />
+        <Toolbar.Button
+          onClick={addSquareNode}
+          className="w-32 h32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2"
+        />
       </Toolbar.Root>
 
     </div>
