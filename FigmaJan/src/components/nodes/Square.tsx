adfd2ce9e08499e89ae2f810b6a9df1187382b02
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-01-30 13:37:01 -0300

    Update

--- FigmaJan/src/components/nodes/Square.tsx
+++ FigmaJan/src/components/nodes/Square.tsx
@@ -3,8 +3,33 @@ import { NodeProps, Handle, Position } from "reactflow";
 export function Square(props: NodeProps) {
     return (
         <div className="bg-violet-500 rounded w-[200px] h-[200px] ">
-            <Handle type="source" id="right" position={Position.Right} />
-            <Handle type="source" id="left" position={Position.Left} />
+            <Handle
+                type="source"
+                id="right"
+                position={Position.Right}
+                className="-right-5 w-3 h-3 bg-blue-400/80"
+            />
+
+            <Handle
+                type="source"
+                id="left"
+                position={Position.Left}
+                className="-left-5 w-3 h-3 bg-blue-400/80"
+            />
+            <Handle
+                type="source"
+                id="top"
+                position={Position.Top}
+                className="-top-5 w-3 h-3 bg-blue-400/80"
+            />
+            <Handle
+                type="source"
+                id="bottom"
+                position={Position.Bottom}
+                className="-bottom-5 w-3 h-3 bg-blue-400/80"
+            />
+
+
         </div>
     )
 }
